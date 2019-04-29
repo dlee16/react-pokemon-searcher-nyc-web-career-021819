c89970c8a05fd586fc331bfc0ee22eb3b262b904
dlee16 <diana.lee116@gmail.com>
2019-04-29 15:59:23 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,39 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state ={ 
+    toggle: true
+  }
+
+  toggleImage = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
+  togglePokemon = () => {
+    if(this.state.toggle){
+      return this.props.pokemon.sprites.front}
+      else {
+        return this.props.pokemon.sprites.back
+    }
+  }
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.toggleImage}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.togglePokemon()} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.stats.find(stat => stat.name === "hp").value} hp
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,23 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+  renderAllPokemon = () => {
+    return this.filterPokemon().map(pokemon => {
+      return (
+        <PokemonCard key={pokemon.id} pokemon={pokemon}/> 
+        )}
+    )
+  }
+
+  filterPokemon = () => {
+    return this.props.pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.props.input))
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {this.renderAllPokemon()}
       </Card.Group>
     )
   }
